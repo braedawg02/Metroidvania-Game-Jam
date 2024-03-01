c564cb37d9a682cadbf979529890a63bd1eb0cff
Braeden Cramer <b@cramerfamily.org>
2024-02-29 20:51:16 -0700

    V0.1.2
    -Added a script for NPC interactions
    -Added a custom font
    -Added a bunch of font sprites, still needs to be converted to a font using https://yal.cc/r/20/pixelfont/
    -Separated sprites from Jon's tile art

---
+++ Apothesis/Assets/Scripts/FeatherScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFeather : MonoBehaviour
+{
+    public static float fAmount = 0; // Global feather amount
+}
+
+public class FeatherScript : MonoBehaviour
+{
+    [SerializeField] public GameObject player; // Reference to the player object
+
+    // Awake is called before the first frame update
+    void Awake()
+    {
+        Debug.Log(GlobalFeather.fAmount); // Log the global feather amount
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Debug.Log("Feather collected"); // Log that the feather is collected
+            GlobalFeather.fAmount++; // Increase the global feather amount
+            gameObject.SetActive(false); // Deactivate the feather game object
+        }
+    }
+}
---
+++ Apothesis/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float smoothness = 0.5f; // Adjust this value to control the smoothness of the camera movement
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate the velocity offset based on the player's horizontal velocity
+        Vector3 velocityOffset = new Vector3(player.GetComponent<Rigidbody2D>().velocity.x, 0, 0);
+        Vector3 targetPosition = player.transform.position + offset + velocityOffset;
+
+        // Calculate the distance between the current camera position and the target position
+        float distance = Vector3.Distance(transform.position, targetPosition);
+
+        // Check if the player is moving
+        if (distance > 0.01f)
+        {
+            // Smoothly move the camera towards the target position
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothness * Time.deltaTime);
+        }
+        else
+        {
+            // If the player is not moving, snap the camera to the target position
+            transform.position = targetPosition;
+        }
+    }
+}
